a982dd872361741e1305d58715c68a5c39caab3e
Theodore Chatziperis <xatzipe@gmail.com>
2015-09-21 11:41:46 +0300

    add comments helping debugging proccess

--- karma.conf.js
+++ karma.conf.js
@@ -1,40 +1,53 @@
 /* jshint node: true */
-
 module.exports = function(config) {
+  "use strict";
+
+/**
+ * add --debug flag to cmd to prevent coverage from
+ * uglifying the js files
+ *
+ * add --single-run=false flag to cmd
+ * to prevent browser fron auto closing
+ */
+    var sourcePreprocessors = 'coverage';
+    function isDebug(argument) {
+        return argument === '--debug';
+    }
+    if (process.argv.some(isDebug)) {
+        sourcePreprocessors = [];
+    }
 
     config.set({
-        basePath: '.',
-        frameworks: ['jasmine'],
+        basePath: ".",
+        frameworks: ["qunit"],
         files: [
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/rfc6570/rfc6570.js',
-            'bower_components/jquery-mockjax/dist/jquery.mockjax.js',
-            'jquery-hal.js',
-            'test/**.js'
+            "bower_components/jquery/dist/jquery.js",
+            "bower_components/rfc6570/rfc6570.js",
+            "bower_components/jquery-mockjax/dist/jquery.mockjax.js",
+            "jquery-hal.js",
+            "test/**.js"
         ],
 
-
-        reporters: ['dots', 'coverage'],
+        reporters: ["dots", "coverage"],
         preprocessors: {
-            'jquery-hal.js': 'coverage'
+            "jquery-hal_new.js": sourcePreprocessors
         },
 
         coverageReporter: {
             reporters: [{
-                type: 'html',
-                dir: 'coverage/'
+                type: "html",
+                dir: "coverage/"
             }, {
-                type: 'text-summary'
+                type: "text-summary"
             }]
         },
 
         proxies: {
-            '/local/': 'http://localhost:8080/'
+            "/local/": "http://localhost:8080/"
         },
 
         autoWatch: false,
         singleRun: true,
-        browsers: ['PhantomJS']
+        browsers: ["PhantomJS"] //, "Chrome", "Firefox", "IE", "ChromeCanary", "Safari", "Opera"]
     });
-
 };
